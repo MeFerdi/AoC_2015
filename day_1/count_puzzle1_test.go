3ea72a96291a195fc33b1581c5714d081194269b
ferdyodhiambo <oferdinanddev112@gmail.com>
2024-08-27 11:55:05 +0300

    Test: implement a unit test for sum function

--- day_1/count_puzzle1_test.go
+++ day_1/count_puzzle1_test.go
@@ -1,3 +1,13 @@
 package main
 
-// func TestSum(*test )
+import "testing"
+
+func TestSum(t *testing.T) {
+	got := int(138)
+	want := int(138)
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+
+}
